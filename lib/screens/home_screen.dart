04746e0e4ae7b36cc1c3efb1dcea35b8aba9a768
Marcos <mbinoti@gmail.com>
2023-06-19 10:38:15 -0300

    Mostrando itens da lista com o widget FirestoreListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:faker/faker.dart';
+import 'package:faker_app_flutter_firebase/data/firestore_repository.dart';
 import 'package:faker_app_flutter_firebase/routing/app_router.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -15,12 +19,40 @@ class HomeScreen extends ConsumerWidget {
           onPressed: () => context.goNamed(AppRoute.profile.name),
         )
       ]),
+      body: const JobsListView(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          debugPrint('Not implemented');
+          final user = ref.read(firebaseAuthProvider).currentUser;
+          final faker = Faker();
+          final title = faker.job.title();
+          final company = faker.company.name();
+          ref.read(firestoreRepositoryProvider).addJob(
+                user!.uid,
+                title,
+                company,
+              );
         },
       ),
+    );
+  }
+}
+
+class JobsListView extends ConsumerWidget {
+  const JobsListView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firestoreRepository = ref.watch(firestoreRepositoryProvider);
+    return FirestoreListView(
+      query: firestoreRepository.jobsQuery(),
+      itemBuilder: (BuildContext context,
+          QueryDocumentSnapshot<Map<String, dynamic>> doc) {
+        return ListTile(
+          title: Text(doc['title']),
+          subtitle: Text(doc['company']),
+        );
+      },
     );
   }
 }
